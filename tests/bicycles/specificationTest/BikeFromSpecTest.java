2805460ff9ee6fddb83a1d7a15c95e2254396644
siyolisa1994 <cyozana@gmail.com>
2019-03-22 12:52:52 +0200

    BikeFromSpec should only test BicycleSpecification

--- tests/bicycles/specificationTest/BikeFromSpecTest.java
+++ tests/bicycles/specificationTest/BikeFromSpecTest.java
@@ -11,38 +11,38 @@ public class BikeFromSpecTest {
     public void shouldTestBicycleSpecificationsForRoadBike(){
 
         BicycleSpecification bike_Spec = new BicycleSpecification(11, -4, BicycleType.RoadBike);
-        System.out.println( "Bike Type" + bike_Spec.getBicycleType() +
-                            "Acceleration Speed: " + bike_Spec.getAccelerationSpeed() +
-                            "Brake Speed: " + bike_Spec.getBrakeSpeed());
+        System.out.println( "\nBike type: " + bike_Spec.getBicycleType() +
+                            "\nAcceleration Speed: " + bike_Spec.getAccelerationSpeed() +
+                            "\nBrake Speed: " + bike_Spec.getBrakeSpeed());
 
-        assertEquals(2, bike_Spec.getAccelerationSpeed());
-        assertEquals(2, bike_Spec.getBrakeSpeed());
-        assertEquals(bike_Spec.getBicycleType(), "Road Bike", toString());
+        assertEquals(11, bike_Spec.getAccelerationSpeed());
+        assertEquals(-4, bike_Spec.getBrakeSpeed());
+        assertEquals(bike_Spec.getBicycleType().toString(), "RoadBike");
     }
     @Test
     public void shouldTestBicycleSpecificationsForMountainBike(){
 
         BicycleSpecification bike_Spec = new BicycleSpecification(5, -3, BicycleType.MountainBike);
-        System.out.println( "Bike Type" + bike_Spec.getBicycleType() +
-                            "Acceleration Speed: " + bike_Spec.getAccelerationSpeed() +
-                            "Brake Speed: " + bike_Spec.getBrakeSpeed());
+        System.out.println( "\nBike type: " + bike_Spec.getBicycleType() +
+                            "\nAcceleration Speed: " + bike_Spec.getAccelerationSpeed() +
+                            "\nBrake Speed: " + bike_Spec.getBrakeSpeed());
 
-        assertEquals(2, bike_Spec.getAccelerationSpeed());
-        assertEquals(2, bike_Spec.getBrakeSpeed());
-        assertEquals(bike_Spec.getBicycleType(), "Mountain Bike", toString());
+        assertEquals(5, bike_Spec.getAccelerationSpeed());
+        assertEquals(-3, bike_Spec.getBrakeSpeed());
+        assertEquals(bike_Spec.getBicycleType().toString(),"MountainBike");
     }
 
     @Test
     public void shouldTestBicycleSpecificationsForTandamBike(){
 
         BicycleSpecification bike_Spec = new BicycleSpecification(12, -7, BicycleType.Tandam);
-        System.out.println( "Bike Type" + bike_Spec.getBicycleType() +
-                            "Acceleration Speed: " + bike_Spec.getAccelerationSpeed() +
-                            "Brake Speed: " + bike_Spec.getBrakeSpeed());
+        System.out.println( "\nBike type: " + bike_Spec.getBicycleType() +
+                            "\nAcceleration Speed: " + bike_Spec.getAccelerationSpeed() +
+                            "\nBrake Speed: " + bike_Spec.getBrakeSpeed());
 
-        assertEquals(2, bike_Spec.getAccelerationSpeed());
-        assertEquals(2, bike_Spec.getBrakeSpeed());
-        assertEquals(bike_Spec.getBicycleType(),"Tandam", toString());
+        assertEquals(12, bike_Spec.getAccelerationSpeed());
+        assertEquals(-7, bike_Spec.getBrakeSpeed());
+        assertEquals(bike_Spec.getBicycleType().toString(),"Tandam");
 
     }
 
